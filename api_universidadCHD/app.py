be74b82a198e55f74a22196b665203aece0eb9ef
MariaDelCarmenHernandezDiaz <50385976+MariaDelCarmenHernandezDiaz@users.noreply.github.com>
2020-02-23 12:50:57 -0600

    Add files via upload

--- api_universidadCHD/app.py
+++ api_universidadCHD/app.py
@@ -3,6 +3,11 @@ import web  # pip install web.py
 
 urls = (
     '/alumnos/?', 'application.controllers.alumnos.Alumnos',
+    '/search/?', 'application.controllers.search.Search',
+    '/delete/?', 'application.controllers.delete.Delete',
+    '/update/?', 'application.controllers.update.Update',
+    '/put/?', 'application.controllers.put.Put',
+
 )
 
 

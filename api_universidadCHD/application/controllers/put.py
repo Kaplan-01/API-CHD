7c936f05c0f89b0dcabbdbedc50162d2caeaf56b
MariaDelCarmenHernandezDiaz <50385976+MariaDelCarmenHernandezDiaz@users.noreply.github.com>
2020-02-27 00:12:56 -0600

    PUT - VERSION 0.3.0

--- api_universidadCHD/application/controllers/put.py
+++ api_universidadCHD/application/controllers/put.py
@@ -1,66 +1,60 @@
-# Version 0.3.0
+#Version 0.3.0
+# EJEMPLO URL 
+# http://localhost:8080/put?action=put&token=4321&matricula=1718110389&nombre=Mela&primer_apellido=Kaplan&segundo_apellido=Salmon&&carrera=DD
 
-import web 
-import app 
-import csv 
-import json 
+import web
+import app
+import csv
+import json
 
 
 class Put:
-    file="/static/csv/alumnos.csv"
-    def __init__(self): 
-        pass 
+    file = "/static/csv/alumnos.csv"
+
+    def __init__(self):
+        pass
 
     def GET(self):
         try:
-            data = web.input() 
-            if data['token'] == "4321": 
-                if data['action'] == 'put':
-                    matricula=data['matricula']
-                    nombre=data['nombre']
-                    primer_apellido=data['primer_apellido']
-                    segundo_apellido=data['segundo_apellido']
-                    carrera=data['carrera']
+            data = web.input()
+            if data['token'] == "4321":
+                if data['action'] == "put":
+                    matricula = data['matricula']
+                    nombre = data['nombre']
+                    primer_apellido = data['primer_apellido']
+                    segundo_apellido = data['segundo_apellido']
+                    carrera = data['carrera']
+                    result2 = self.actionPut(
+                        self.file, matricula, nombre, primer_apellido, segundo_apellido, carrera)
+                    return json.dumps(result2)
 
-                    bus2 = self.actionPut(self.file,matricula,nombre,primer_apellido,segundo_apellido,carrera) 
-                    return json.dumps(bus2) 
-                else:
-                    bus2 = {} 
-                    bus2['status'] = "Command not found"
-                    return json.dumps(bus2) 
-            else:
-                bus = {} 
-                bus['bus'] = "Invalid Token"
-                return json.dumps(bus) 
         except Exception as e:
-            bus = {} 
-            bus['status'] = "Values missing, sintaxis: alumnos?action=get&token=XXXX"
-            return json.dumps(bus) 
+            result = {}
+            result['status'] = "Values missing"
+            return json.dumps(result)
 
     @staticmethod
-    def actionPut(file, matricula,nombre,primer_apellido,segundo_apellido,carrera):
+    def actionPut(file, matricula, nombre, primer_apellido, segundo_apellido, carrera):
         try:
-            busR = []
-            bus = []
-            bus2 = {} 
-            busR.append(matricula)
-            busR.append(nombre)
-            busR.append(primer_apellido)
-            busR.append(segundo_apellido)
-            busR.append(carrera)
-            with open('static/csv/alumnos.csv','n', nuevoRegistro='') as csvfile: 
+            result = []
+            resultR = []
+            result2 = {}
+            resultR.append(matricula)
+            resultR.append(nombre)
+            resultR.append(primer_apellido)
+            resultR.append(segundo_apellido)
+            resultR.append(carrera)
+            with open('static/csv/alumnos.csv', 'a', newline='') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow(busR)
-            with open('static/csv/alumnos.csv','r') as csvfile:
+                writer.writerow(resultR)
+            with open('static/csv/alumnos.csv', 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
-                for row in reader: 
-                    bus.append(row)
-                    bus2['status']="200 Ok"
-                    bus2['alumnos']=bus
-            return bus2 
-        except  Exception as e:
-            bus = {}
-            bus['status'] = "Error"
-            return bus 
-
-# http://localhost:8080/put?action=put&token=4321&matricula=1718110381&nombre=Carmn&primer_apellido=Kapan&segundo_apellido=Daz&carrera=IN
+                for row in reader:
+                    result.append(row)
+                    result2['status'] = "200 Ok"
+                    result2['alumnos'] = result
+            return result2
+        except Exception as e:
+            result = {}
+            result['status'] = "Error"
+        return result

4a2609e016e0e0120b745d1e5c82456e3cbb3a70
MariaDelCarmenHernandezDiaz <50385976+MariaDelCarmenHernandezDiaz@users.noreply.github.com>
2020-02-27 00:14:35 -0600

    SEARCH - VERSION 0.2.0

--- api_universidadCHD/application/controllers/search.py
+++ api_universidadCHD/application/controllers/search.py
@@ -1,55 +1,59 @@
+# EJEMPLO URL 
+# http://localhost:8080/search?action=search&token=4321&matricula=1718110389
+
 # Version 0.2.0
 
-import web 
-import app 
-import csv 
-import json 
+import web
+import app
+import csv
+import json
 
 
 class Search:
-    file="/static/csv/alumnos.csv"
-    def __init__(self): 
-        pass 
+    file = "/static/csv/alumnos.csv"
+
+    def __init__(self):
+        pass
 
     def GET(self):
         try:
-            data = web.input() 
-            if data['token'] == "4321": 
+            data = web.input()
+            if data['token'] == "4321":
                 if data['action'] == 'search':
-                    matricula=data['matricula']
-                    bus2 = self.actionSearch(self.file,matricula) 
-                    return json.dumps(bus2) 
+                    matricula = data['matricula']
+                    bus2 = self.actionSearch(self.file, matricula)
+                    return json.dumps(bus2)
                 else:
-                    bus2 = {} 
+                    bus2 = {}
                     bus2['status'] = "Command not found"
-                    return json.dumps(bus2) 
+                    return json.dumps(bus2)
             else:
-                bus = {} 
+                bus = {}
                 bus['bus'] = "Invalid Token"
-                return json.dumps(bus) 
+                return json.dumps(bus)
         except Exception as e:
-            bus = {} 
-            bus['status'] = "Values missing, sintaxis: alumnos?action=get&token=XXXX"
-            return json.dumps(bus) 
+            bus = {}
+            bus['status'] = "Values missing"
+            return json.dumps(bus)
 
     @staticmethod
     def actionSearch(file, matricula):
         try:
             bus = []
-            bus2 = {} 
-            with open('static/csv/alumnos.csv','r') as csvfile: 
-                reader = csv.DictReader(csvfile) 
-                for row in reader: 
-                    if(matricula==row['matricula']):
+            bus2 = {}
+            with open('static/csv/alumnos.csv', 'r') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    if(matricula == row['matricula']):
                         bus.append(row)
-                        bus2['status']="200 Ok"
-                        bus2['alumnos']=bus
+                        bus2['status'] = "200 Ok"
+                        bus2['alumnos'] = bus
                         break
-                    else: 
-                        bus2={}
-                        bus2['status']="No esta registrado"
-            return bus2 
-        except  Exception as e:
+                    else:
+                        bus2 = {}
+                        bus2['status'] = "No esta registrado"
+            return bus2
+        except Exception as e:
             bus = {}
             bus['status'] = "Error"
-            return bus 
+            return bus
